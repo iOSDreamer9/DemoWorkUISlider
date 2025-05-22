03c95a95325334f6d5e96467acaeeecf3a32e3d9
iOSDreamer9 <pocemoon006@gmail.com>
2025-05-22 23:15:49 +0300

    added func calculated:Test1

--- DemoWorkUISlider/ViewController.swift
+++ DemoWorkUISlider/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelHeight: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var labelWeight: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
+    @IBOutlet weak var labelResult: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +24,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        labelHeight.text = String(format: "%.1f", sender.value)
+        let height = String(format: "%.1f", sender.value)
+        labelHeight.text = "\(height) m"
         print(sender.value)
     }
+    @IBAction func weightSliderChanger(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        labelWeight.text = weight
+    }
     
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / (height * height)
+        labelResult.text = "Ваш результат: \(bmi)"
+        print(bmi)
+        
+    }
 }
 
